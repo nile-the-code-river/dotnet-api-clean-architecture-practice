2430d7569bed9ef6a84147fda76a88da0f69f927
Nile Kim <nile.coderiver@gmail.com>
2025-03-25 16:55:04 +0900

    inject dependencies in the API project

---
+++ CleanArchitecturePractice.API/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using ClearnArchitecturePractice.Application;
+using ClearnArchitecturePractice.Infrastructure;
+
+namespace CleanArchitecturePractice.API
+{
+    public static class DependencyInjection
+    {
+        // 'this' as parameter makes this an 'extension method'
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                .AddInfrastructureDI();
+
+            return services;
+        }
+    }
+}
--- CleanArchitecturePractice.API/Program.cs
+++ CleanArchitecturePractice.API/Program.cs
@@ -1,3 +1,5 @@
+using CleanArchitecturePractice.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add SERVICES to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// using CleanArchitecturePractice.API;
+builder.Services.AddAppDI();
 
 var app = builder.Build();
 
