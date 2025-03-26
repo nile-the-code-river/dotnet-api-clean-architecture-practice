de8eabf8a91d468003409f346edfaacfd573df17
Nile Kim <nile.coderiver@gmail.com>
2025-03-26 17:04:37 +0900

    implement basic structure

---
+++ ClearnArchitecturePractice.Core/Database/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearnArchitecturePractice.Core.Database
+{
+    public class Account
+    {
+        public required int Id { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; } // hash
+        public required string Name { get; set; }
+        public required string Company { get; set; }
+        public required string Country { get; set; }
+        public required string PrimaryAddress { get; set; }
+        public required string SecondaryAddress { get; set; }
+        public required bool IsActive { get; set; }
+        public required DateTimeOffset RegisteredDate { get; set; }
+
+        // public required AccountRole Role {get;set;}
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # dotnet-api-clean-architecture-practice
 
+short, simple, basic
+
 https://www.youtube.com/watch?v=sBAB_EKYPYs&ab_channel=WebGentle
+// good to have separate dependency files for each layer (app, domain_core, infra)
+
+
+DI_dependency injection
+
+
+
+Domain == Core
+    // = Contains core classes (=DTO_data transfer object, Entities_classes for entity framework, Enums)
+    // = Business logic
+    // (= commands for MediatoR)
+
+Infrastructure
+    // = Contains main implementations
+    // such as...
+        // 1. repository implementation for injecting a database
+        // 2. Db config for entity framework core
+        // 3. 3rd party sth
+    // = Used when implementing something that interacts with external file
